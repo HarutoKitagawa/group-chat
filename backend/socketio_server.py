23fbdcd18cbc07caf601674ae7b54912fdf62849
Haruto Kitagawa <h.kitagawa.12@icloud.com>
2023-06-18 21:50:06 +0900

    TextList作成

--- backend/socketio_server.py
+++ backend/socketio_server.py
@@ -5,14 +5,14 @@ import uvicorn
 
 app_fastapi = FastAPI()
 
-# origins = [
-#     'http://localhost:3000',
-#     'http://127.0.0.1:3000'
-# ]
+origins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+]
 
 app_fastapi.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,10 +24,6 @@ app_socketio = socketio.ASGIApp(sio, other_asgi_app=app_fastapi)
 @sio.on("message")
 async def echo(sid, data):
     await sio.emit("message", data)
-    
-@sio.event
-async def connect(sid, environ):
-    print("connected", sid)
 
 if __name__ == "__main__":
     uvicorn.run(app_socketio, host="127.0.0.1", port=8000)
--- frontend/src/components/TextList.tsx
+++ frontend/src/components/TextList.tsx
@@ -1,15 +1,21 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect} from "react"
 import { SocketContext } from "../providers/SocketProvider"
 
 export const TextList = () => {
     const ws = useContext(SocketContext)
+    const [textList, setList] = useState<string[]>([""])
 
-    if (ws) {
-        ws.on("message", (data) => console.log(data))
-    }
+    useEffect(() => {
+        if (ws) {
+            ws.on("message", (data) => {
+                setList((prevTextList) => [...prevTextList, data])
+            })
+        }
+    },[])
 
     return (
         <ul>
+            {textList.map((text, index) => <li key={index}>{text}</li>)}
         </ul>
     )
 }
